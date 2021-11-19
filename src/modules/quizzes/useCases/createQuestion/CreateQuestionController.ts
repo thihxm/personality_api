43242caeca262778d91e2f23492f1d58db1310ac
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-11-19 20:43:56 -0300

    feat: create question route

--- src/modules/quizzes/useCases/createQuestion/CreateQuestionController.ts
+++ src/modules/quizzes/useCases/createQuestion/CreateQuestionController.ts
@@ -1,1 +1,21 @@
-class CreateQuestionController {}
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { CreateQuestionUseCase } from './CreateQuestionUseCase'
+
+class CreateQuestionController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { label, quiz_id } = req.body
+
+    const createQuestionUseCase = container.resolve(CreateQuestionUseCase)
+
+    const question = await createQuestionUseCase.execute({
+      label,
+      quiz_id,
+    })
+
+    return res.status(201).json(question)
+  }
+}
+
+export { CreateQuestionController }
