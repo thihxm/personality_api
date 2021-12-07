0b7ffe4acd6a053979c4bca3a5b2a1c12ce6481b
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-12-07 14:15:40 -0300

    feat: UsersResults routes ensure authenticated

--- src/shared/infra/http/routes/usersResults.routes.ts
+++ src/shared/infra/http/routes/usersResults.routes.ts
@@ -3,12 +3,22 @@ import { Router } from 'express'
 import { CreateUserResultController } from '@modules/usersResults/useCases/createUserResult/CreateUserResultController'
 import { ListUserResultsController } from '@modules/usersResults/useCases/listUserResults/ListUserResultsController'
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+
 const usersResultsRoutes = Router()
 
 const createUserResultController = new CreateUserResultController()
 const listUserResultsController = new ListUserResultsController()
 
-usersResultsRoutes.post('/', createUserResultController.handle)
-usersResultsRoutes.get('/', listUserResultsController.handle)
+usersResultsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  createUserResultController.handle
+)
+usersResultsRoutes.get(
+  '/',
+  ensureAuthenticated,
+  listUserResultsController.handle
+)
 
 export { usersResultsRoutes }
